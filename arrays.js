a942a3ba76c511f80e8047ea3aef7998d4564c28
Josh Kramer <joshtkramer@yahoo.com>
2017-10-23 18:42:33 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -44,3 +44,8 @@ function destructivelyRemoveElementFromEndOfArray(array){
   array.pop();
   return array;
 }
+
+function removeElementFromEndOfArray(array){
+  var array = [1, 2, 3];
+  array.slice(0, array.length - 1);
+}
